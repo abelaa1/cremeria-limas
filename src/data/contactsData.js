1f6130b2476f4eaa6e10613b8586b6a2bf09ce1c
Abel <aguilarabel165@gmail.com>
2022-09-03 18:33:09 -0700

    slideshow

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,5 +1,5 @@
 export const contactsData = {
-    email: 'Omar.Arteaga@gmail.com',
+    email: 'artagaomar@gmail.com',
     phone: '3535356342',
     address: 'Altamirano Pte. 162, Guadalupe Victoria, 59500 San José de Gracia, Mich.',
 
