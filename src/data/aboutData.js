5aa80318d3f1ca256d0188f079b0eca472fe380e
Abel <aaguilar61@gatech.edu>
2024-10-15 03:32:57 -0700

    code keep up

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -3,6 +3,6 @@ import logo from './mexico.PNG';
 export const aboutData = {
     title: "Quién Soy",
     description1: "Yo soy Omar Arteaga. ",
-    description2: "Ven y conoce mi negocio! ",
+    description2: "Y vengo de muy humildes comienzos. Con mucho trabajo y dedicación, he logrado construir el negocio que hoy tenemos. Siempre he tenido a mi familia como pilar fundamental, y ellos son los que me motivan a seguir adelante. ¡Ven y conoce lo que tenemos para ofrecer! ",
     image: logo
 }
